55c768047056233398ef9094ed43fadb8d782ffc
Hannah Squier <hannahsquier@gmail.com>
2016-07-08 09:26:24 -0700

    write initialize test and add test

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,1 +1,22 @@
 # Your code here
+require 'calculator'
+
+describe Calculator do 
+
+  let(:calculator) { Calculator.new }
+  let(:a) { 4 }
+  let(:b) { 2 }
+
+  describe "#initialize" do
+    it "instantiates a Calculator object" do
+      expect { Calculator.new }.not_to raise_error
+    end
+  end
+
+  describe "#add" do
+    it "adds two numbers correctly" do
+      expect(calculator.add(a, b)).to eq(6)
+    end
+  end
+
+end
